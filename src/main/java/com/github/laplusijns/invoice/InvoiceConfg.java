c8c98f1bdd647c8fae130b807f7e2d68ba2e4573
ygs <ygs@DESKTOP-RUT8DG2>
2026-08-27 15:05:33 +0800

    調整設定

--- src/main/java/com/github/laplusijns/invoice/InvoiceConfg.java
+++ src/main/java/com/github/laplusijns/invoice/InvoiceConfg.java
@@ -34,9 +34,10 @@ public class InvoiceConfg {
         final var openAiChatOptions = OpenAiChatOptions.builder()
                 .responseFormat(ResponseFormat.builder().type(Type.JSON_OBJECT).build())
                 .model(optionsModel)
-                .temperature(0.0)
+                .temperature(1.0)
                 .apiKey(apiKey)
                 .baseUrl(baseUrl)
+                .reasoningEffort("high")
                 .build();
 
         return OpenAiChatModel.builder().options(openAiChatOptions).build();
